69f7eb9fd6654aae291f60e226d91c3bab29adb5
Lukáš Hanák <lukas.hanak@protonmail.com>
2020-05-02 19:13:17 +0200

    idk

---
+++ app.js
@@ -0,0 +1,18 @@
+// operators
+const divideBtn = document.getElementById("divide");
+const multiplyBtn = document.getElementById("multiply");
+const subtractBtn = document.getElementById("subtract");
+const addBtn = document.getElementById("add");
+const equalsBtn = document.getElementById("add");
+
+// numbers 
+const oneBtn = document.getElementById("one");
+const twoBtn = document.getElementById("two");
+const threeBtn = document.getElementById("three");
+const fourBtn = document.getElementById("four");
+const fiveBtn = document.getElementById("five");
+const sixBtn = document.getElementById("six");
+const sevenBtn = document.getElementById("seven");
+const eightBtn = document.getElementById("eight");
+const nineBtn = document.getElementById("nine");
+
